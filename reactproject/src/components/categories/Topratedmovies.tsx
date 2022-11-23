e80b0e5cb7e85d4a9ed9deeec70ce06247484c9c
lberthaut <you@example.com>
2022-11-23 17:18:23 +0100

    add details title

--- reactproject/src/components/categories/Topratedmovies.tsx
+++ reactproject/src/components/categories/Topratedmovies.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react"
-import Fetchmovie from "../../services/Fetchmovie"
+import { useState } from "react";
+import Fetchmovie from "../../services/Fetchmovie";
 import List from "../List";
 import { Imovie } from "../Home";
-import { Idatas } from '../Home'
-
+import { Idatas } from "../Home";
 
 export default function Topratedmovies() {
-    const [datas, setDatas] = useState<Idatas | null>(null)
-    const topratedTitle = "Films les mieux notés"
-    const topratedURL = '/movie/top_rated'
+  const [datas, setDatas] = useState<Idatas | null>(null);
+  const topratedTitle = "Films les mieux notés ever";
+  const topratedURL = "/movie/top_rated";
 
-
-    return (
-        <>
-            <Fetchmovie setDatas={setDatas} URI={topratedURL} />
-            <List datas={datas} titlecontainer={topratedTitle} />
-        </>)
+  return (
+    <>
+      <Fetchmovie setDatas={setDatas} URI={topratedURL} />
+      <List datas={datas} titlecontainer={topratedTitle} />
+    </>
+  );
 }
